3455e6f469b33512032be7d47774c6c561f8ecdf
纷羽 <gzj0078@icloud.com>
2019-02-26 18:21:53 +0800

    撤销上一个更改

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,82 +5,65 @@ module.exports = {
   mode: 'universal',
 
   /*
-   ** Headers of the page
-   */
+  ** Headers of the page
+  */
   head: {
     title: `${master}'s Blob`,
-    meta: [{
-        charset: 'utf-8'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: pkg.description
-      }
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico'
-    }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
   },
 
   /*
-   ** Customize the progress-bar color
-   */
-  loading: {
-    color: '#fff'
-  },
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
 
   /*
-   ** Global CSS
-   */
+  ** Global CSS
+  */
   css: [
     'element-ui/lib/theme-chalk/index.css'
   ],
 
   /*
-   ** Plugins to load before mounting the App
-   */
+  ** Plugins to load before mounting the App
+  */
   plugins: [
     '@/plugins/element-ui'
   ],
 
   /*
-   ** Nuxt.js modules
-   */
+  ** Nuxt.js modules
+  */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
   ],
   /*
-   ** Axios module configuration
-   */
+  ** Axios module configuration
+  */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
   },
 
   /*
-   ** Build configuration
-   */
+  ** Build configuration
+  */
   build: {
     transpile: [/^element-ui/],
-
+    
     /*
-     ** You can extend webpack config here
-     */
-    extend(config, {
-      isDev,
-      isClient
-    }) {
-      if (!isDev) {
-        config.output.publicPath = './_nuxt/';
-      }
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      
     }
   }
 }
